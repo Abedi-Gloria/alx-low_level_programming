5870b537356b0e7b4171539d5fdb21bdabd6faeb
Abedi-Gloria <akosuagyemfa98@gmail.com>
2022-03-16 17:57:44 -0700

    contains function prototype

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,15 @@
 #include "main.h"
 /**
-*	print_alphabet: a function that prints characters in lower case
+*	Description: print_alphabet - a function that prints characters in lower case
 *	Description: Uses the holberton header file, that brings in the _putchar
 *	Return: returns void.
 */
 void print_alphabet(void)
 	{
 	char low;
+
 	low = 'a';
+
 	while (low <= 'z')
 	{
 		_putchar(low);
