02e53722458628536365e27b48c002249bc8f151
Abedi-Gloria <akosuagyemfa98@gmail.com>
2022-03-23 01:22:00 +0000

    Pointers

--- 0x05-pointers_arrays_strings/0-main.c
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include "0-reset_to_98.c"
 #include "1-swap.c"
-
+#include "2-strlen.c"
 /**
  * main - check the code
  * 
@@ -23,15 +23,26 @@ int test()
  *
  * Return: Always 0.
  */
+// int swap()
+// {
+//     int a;
+//     int b;
+
+//     a = 98;
+//     b = 42;
+//     printf("a=%d, b=%d\n", a, b);
+//     swap_int(&a, &b);
+//     printf("a=%d, b=%d\n", a, b);
+//     return (0);
+// }
+
 int main(void)
 {
-    int a;
-    int b;
+    char *str;
+    int len;
 
-    a = 98;
-    b = 42;
-    printf("a=%d, b=%d\n", a, b);
-    swap_int(&a, &b);
-    printf("a=%d, b=%d\n", a, b);
+    str = "My first strlen!";
+    len = _strlen(str);
+    printf("%d\n", len);
     return (0);
 }
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * @_strlen - function for determinig length of a string 
+ * 
+ * @s: a string of characters
+ * @i: int for counting
+ * @return: returns the value of i
+ */
+
+int _strlen(char *s)
+{
+	int i =0;
+	for(; *s != '\0'; s++)
+	{
+		i++;
+	}
+	return (i);
+}
