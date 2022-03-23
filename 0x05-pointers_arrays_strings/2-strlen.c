694ef2f257f7952e9683712d93a67dbd8b5722e1
Abedi-Gloria <akosuagyemfa98@gmail.com>
2022-03-23 01:28:49 +0000

    Pointers

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -5,8 +5,7 @@
  * _strlen - function for determinig length of a string
  *
  * @s: a string of characters
- * @i: int for counting
- * @return: returns the value of i
+ * Return: returns the value of i
  */
 
 int _strlen(char *s)
@@ -14,6 +13,7 @@ int _strlen(char *s)
 	int i = 0;
 	for (; *s != '\0'; s++)
 	{
+		
 		i++;
 	}
 	return (i);
