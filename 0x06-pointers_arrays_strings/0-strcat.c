e16b2785f368d0182af47956a71401c5b1ff14d1
Abedi-Gloria <akosuagyemfa98@gmail.com>
2022-03-25 00:34:32 +0000

    function that concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -17,11 +17,8 @@ char *_strcat(char *dest, char *src)
 	{
 		i++;
 	}
-	for (j = 0; src[j] != 0; j++)
+	for (j = 0; (dest[i + j] = *src++) != '\0'; j++)
 	{
-		dest[i] = src[j];
-		dest++;
 	}
-	dest[i];
 	return(dest);
 }
